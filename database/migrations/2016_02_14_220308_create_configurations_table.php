b3bcd82e343293963ac34c9c39e06ea8089e91f2
masterclass27 <azriatuf@gmail.com>
2017-11-03 17:12:08 +0800

    initial commit

---
+++ database/migrations/2016_02_14_220308_create_configurations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfigurationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configurations', function($table) {
+            $table->increments('id');
+            $table->integer('time_interval_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
